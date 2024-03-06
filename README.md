815090b8ff27a0b1395f04d70f97e518ddc75b25
Steven Lu <ste900811@gmail.com>
2024-03-06 10:19:02 -0500

    set up instruction on MacOS and test pickle if it's working

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This project is built up with a Flask virtual environment, once download the repository, you will have to install the flask virtual environment in your machine, the steps follow:
+    (On MacOS)
+1. check python3 --version and flask --version, install the package if it's not installed
+2. run "virtualenv ." in the directory to build up the virtual enviroment
+3. run "source ./bin/activate" to activate the virtual enviroment
+4. run "pip install flask" to install flask in enviroment
+5. run "pip install pandas", "pip install -U scikit-learn"
--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, redirect, url_for
-import joblib
+import pickle
 import pandas as pd
 
 app = Flask(__name__)
 
-with open("model_joblib.pkl", "rb") as f:
-  clf_joblib = joblib.load(f)
+with open("model_pickle.pkl", "rb") as f:
+  clf = pickle.load(f)
 data = [[1004,1.41,2,50,1,1,66.66666667,104,82,42.6,31,204,113,185,117.0147731,2,2,2,2]]
 test = pd.DataFrame(data, columns=['ParticipantID', 'Income', 'Sex', 'Age', 'Race', 'Edu', 'Diastolic', 'Systolic', 'Pulse', 'BMI', 'HDL', 'Trig', 'LDL', 'TCHOL', 'kidneys_eGFR', 'Diabetes', 'CurrentSmoker', 'isActive', 'isInsured'])
 finalTest = test.drop(columns=["ParticipantID", "Income", "Edu", "isInsured"])
+result = clf.predict_proba(finalTest)
 
 @app.route('/')
 def home():
@@ -16,7 +17,7 @@ def home():
 
 @app.route('/<name>')
 def user(name):
-  return f"Hello {name}!"
+  return f"Hello {name}! {result}"
 
 @app.route('/admin')
 def admin():
