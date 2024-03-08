587da79076b81ea2c9dff8c8e2c483e2dbb606b3
Yiyang Lu <ste900811@gmail.com>
2024-03-07 20:20:21 -0500

    Remove the testList and fix dictionary with string type

--- app.py
+++ app.py
@@ -11,24 +11,19 @@ columns=['Sex', 'Age', 'Race', 'Diastolic', 'Systolic', 'Pulse', 'BMI', 'HDL', '
 startingValue = [2,50,1,66.66666667,104,82,42.6,31,204,113,185,117.0147731,2,2,2]
 startingData = dict(zip(columns, startingValue))
 
-testlist = [1,2,3]
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
   if request.method == 'POST':
     data = dict(request.form)
-    input = [[int(data['Sex']), int(data['Age']), int(data['Race']), float(data['Diastolic']), 
+    input = [int(data['Sex']), int(data['Age']), int(data['Race']), float(data['Diastolic']), 
               float(data['Systolic']), float(data['Pulse']), float(data['BMI']), int(data['HDL']), 
               int(data['Trig']), int(data['LDL']), int(data['TCHOL']), float(data['kidneys_eGFR']), 
-              float(data['Diabetes']), int(data['CurrentSmoker']), float(data['isActive'])]]
-    test = pd.DataFrame(input, columns=columns)
+              float(data['Diabetes']), int(data['CurrentSmoker']), float(data['isActive'])]
+    test = pd.DataFrame([input], columns=columns)
     result = clf.predict_proba(test)
-    print(data)
-    print(data['Sex'])
-    return render_template('home.html', result=result[0][0], data=data, li = testlist)
-  print(startingData)
-  print(startingData['Sex'])
-  return render_template('home.html', result="", data=startingData, li = testlist)
+    dictData = dict(zip(columns, input))
+    return render_template('home.html', result=result[0][0], data=dictData)
+  return render_template('home.html', result="", data=startingData)
 
 if __name__ == '__main__':
   app.run(debug=True)
