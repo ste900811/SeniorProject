0c1743364a96a5911c0adc575fd20bd1a7afad44
Yiyang Lu <ste900811@gmail.com>
2024-03-07 20:43:09 -0500

    Fix all the input to keep user input, and change the result output

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 with open("model_pickle.pkl", "rb") as f:
   clf = pickle.load(f)
 columns=['Sex', 'Age', 'Race', 'Diastolic', 'Systolic', 'Pulse', 'BMI', 'HDL', 'Trig', 'LDL', 'TCHOL', 'kidneys_eGFR', 'Diabetes', 'CurrentSmoker', 'isActive']
-startingValue = [2,50,1,66.66666667,104,82,42.6,31,204,113,185,117.0147731,2,2,2]
+startingValue = [2,50,1,66.67,104.0,82.0,42.6,31,204,113,185,117.01,2,2,2]
 startingData = dict(zip(columns, startingValue))
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,9 +20,15 @@ def home():
               int(data['Trig']), int(data['LDL']), int(data['TCHOL']), float(data['kidneys_eGFR']), 
               float(data['Diabetes']), int(data['CurrentSmoker']), float(data['isActive'])]
     test = pd.DataFrame([input], columns=columns)
-    result = clf.predict_proba(test)
+
+    if clf.predict_proba(test)[0][0] >= 0.5:
+      result = "High percentage has heart attack, check with doctor"
+    else:
+      result = "Low percentage has heart attack, keep healthy"
+    
     dictData = dict(zip(columns, input))
-    return render_template('home.html', result=result[0][0], data=dictData)
+    
+    return render_template('home.html', result=result, data=dictData)
   return render_template('home.html', result="", data=startingData)
 
 if __name__ == '__main__':
