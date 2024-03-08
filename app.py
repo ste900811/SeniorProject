52a2f99eea0083f1354456631b7171080e933106
Yiyang Lu <ste900811@gmail.com>
2024-03-07 20:49:26 -0500

    Documented the code

--- app.py
+++ app.py
@@ -4,14 +4,27 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# Load the model
 with open("model_pickle.pkl", "rb") as f:
   clf = pickle.load(f)
+
+# Columns of the dataset needed for the model
 columns=['Sex', 'Age', 'Race', 'Diastolic', 'Systolic', 'Pulse', 'BMI', 'HDL', 'Trig', 'LDL', 'TCHOL', 'kidneys_eGFR', 'Diabetes', 'CurrentSmoker', 'isActive']
+
+# Starting values for the input form
 startingValue = [2,50,1,66.67,104.0,82.0,42.6,31,204,113,185,117.01,2,2,2]
+
+# Create a dictionary with the columns and starting values
 startingData = dict(zip(columns, startingValue))
 
+# Create the home page with the input form
 @app.route('/', methods=['GET', 'POST'])
 def home():
+
+  # If the form is submitted, which means the request method is POST, we will get the data from the form and store in data variable
+  # Then we will create a dataframe with the input data and set test to the pandas format prepare to training
+  # Use the model to predict the probability of having a heart attack, if probability is greater than 0.5, the result will be "High", otherwise "Low"
+  # Create a dictionary with the input data and return the home page with the result and the input data
   if request.method == 'POST':
     data = dict(request.form)
     input = [int(data['Sex']), int(data['Age']), int(data['Race']), float(data['Diastolic']), 
@@ -28,6 +41,7 @@ def home():
     dictData = dict(zip(columns, input))
     
     return render_template('home.html', result=result, data=dictData)
+  
   return render_template('home.html', result="", data=startingData)
 
 if __name__ == '__main__':
